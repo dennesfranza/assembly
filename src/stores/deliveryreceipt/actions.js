dd2a3f267065eb0c751ed212b1eeb1348f7fdf05
Dennes Franza <dennesfranza@gmail.com>
2024-06-04 12:55:42 +0800

    update delivery store

--- src/stores/deliveryreceipt/actions.js
+++ src/stores/deliveryreceipt/actions.js
@@ -1,7 +1,54 @@
 import { axiosInstance } from "boot/axios";
+import { useRequisitionStore } from "src/stores/requisition/index";
 
-const actionGetAllDeliveryReceiptItems = (state) => {}
+const actionGetAllDeliveryReceiptItems = (state) => {
+  state.listallitemstableloading = true
+  axiosInstance.get(`deliveryreceipt/`).then(response => {
+    console.log(response)
+  }).catch(error => {
+    console.log(error)
+  }).finally(() => {
+    state.listallitemstableloading = false
+  })
+};
+
+const actionOpenDeliveryItemDialog = (state) => {
+  state.deliveryreceiptdialog = true;
+};
+
+const actionCloseDeliveryItemDialog = (state) => {
+  const requisitionstore = useRequisitionStore()
+  state.deliveryreceiptdialog = false
+  requisitionstore.rsnumbersearchresults = []
+  state.deliveryreceiptpayloaditem = {
+    rs_number: null,
+    rs_number_label: null,
+    quantity: null,
+    quantity_int: null,
+    description: null,
+  }
+};
+
+const addItemToCreateDeliveryReceipt = (state) => {
+  let data = state.delivery_receipt_item
+  state.deliveryreceiptpayload.delivery_receipt_items.push(data)
+}
+
+const actionPostDeliveryReceiptItem = (state) => {
+  state.postdeliveryreceiptitemloading = true
+  axiosInstance.post('deliveryreceipt/', state.deliveryreceiptpayload).then(response => {
+    console.log(response)
+  }).catch(error => {
+    console.log(error)
+  }).finally(() => {
+    state.postdeliveryreceiptitemloading = false
+  })
+}
 
 export {
-
-}
+  actionGetAllDeliveryReceiptItems,
+  actionOpenDeliveryItemDialog,
+  actionCloseDeliveryItemDialog,
+  addItemToCreateDeliveryReceipt,
+  actionPostDeliveryReceiptItem
+};
--- src/stores/deliveryreceipt/index.js
+++ src/stores/deliveryreceipt/index.js
@@ -1,11 +1,28 @@
 import { defineStore } from "pinia";
 import state from "./state";
-
+import {
+  actionOpenDeliveryItemDialog,
+  actionCloseDeliveryItemDialog,
+  addItemToCreateDeliveryReceipt,
+  actionPostDeliveryReceiptItem
+} from './actions'
 
 export const useDeliveryReceiptStore = defineStore("deliveryreceipt", {
   state: () => state,
   getters: {},
   actions: {
-
+    openAddDeliveryDialog() {
+      actionOpenDeliveryItemDialog(this)
+    },
+    closeAddDeliveryDialog() {
+      actionCloseDeliveryItemDialog(this)
+    },
+    addDeliveryItem() {
+      addItemToCreateDeliveryReceipt(this)
+      actionCloseDeliveryItemDialog(this)
+    },
+    postItem() {
+      actionPostDeliveryReceiptItem(this)
+    }
   }
 })
